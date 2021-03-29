0985c5e3fb9a45dbded8295e1721999f02b025f4
hyeonic <evan3566@gmail.com>
2021-03-29 23:53:40 +0900

    feat: 개발환경 구축
    java 11
    gradle
    spring boot 2.4.4
    spring-boot-starter-web
    spring-boot-starter-data-jpa
    spring-boot-starter-boot-devtools
    lombok
    h2 database

---
+++ src/main/java/com/healthner/healthner/HealthnerApplication.java
@@ -0,0 +1,13 @@
+package com.healthner.healthner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthnerApplication.class, args);
+    }
+
+}
