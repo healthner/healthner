5c8a79d956fe2b99afd724ab8cdfc0363f18db6f
hyeonic <evan3566@gmail.com>
2021-04-05 14:27:31 +0900

    feat: 기존에 적용한 타임리프 삭제 및 정리
    뷰 정리를 위한 라이브러리 정리 및 화면단 정리

--- src/main/java/com/healthner/healthner/controller/HealthnerController.java
+++ src/main/java/com/healthner/healthner/controller/HealthnerController.java
@@ -3,96 +3,83 @@ package com.healthner.healthner.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class HealthnerController {
-
-
-    @GetMapping(value = "/home")
-    public String home(){
-        return "home";
-    }
-
-    @GetMapping(value = "/search")
-    public String searchGym(){
-        return "search";
-    }
-
-    @GetMapping(value = "/purchase")
-    public String purchase(){
-        return "purchase";
-    }
-
-    @GetMapping(value = "/gymdetail")
-    public String gymDetail(){
-        return "gymdetail";
-    }
-
-    @GetMapping(value = "/reservation")
-    public String reservation(){
-        return "reservation";
-    }
-
-
-
-
-    //출석체크
-    @GetMapping(value = "/gym-mypage/check")
-    public String check(){
-        return "check";
-    }
-
-
-    //마이페이지
-    @GetMapping(value = "/trainer-mypage")
-    public String trainerMypage(){
-        return "trainer-mypage";
-    }
-
-    @GetMapping(value = "/user-mypage")
-    public String userMypage(){
-        return "user-mypage";
-    }
-
-    @GetMapping(value = "/gym-mypage")
-    public String gymMypage(){
-        return "gym-mypage";
-    }
-
-
-
-
-    //등록*생성 (시설,트레이너,상품)
-    @GetMapping(value = "/user-mypage/new-gym")
-    public String newGym(){
-        return "new-gym";
-    }
-
-    @GetMapping(value = "/user-mypage/new-trainer")
-    public String newTrainer(){
-        return "new-trainer";
-    }
-
-    @GetMapping(value = "/gym-mypage/new-product")
-    public String newProductByGym(){
-        return "new-product";
-    }
-
-    @GetMapping(value = "/trainer-mypage/new-product")
-    public String newProductByTrainer(){
-        return "new-product";
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+//@Controller
+//public class HealthnerController {
+//
+//
+//    @GetMapping(value = "/home")
+//    public String home(){
+//        return "home";
+//    }
+//
+//    @GetMapping(value = "/search")
+//    public String searchGym(){
+//        return "search";
+//    }
+//
+//    @GetMapping(value = "/purchase")
+//    public String purchase(){
+//        return "purchase";
+//    }
+//
+//    @GetMapping(value = "/gymdetail")
+//    public String gymDetail(){
+//        return "gymdetail";
+//    }
+//
+//    @GetMapping(value = "/reservation")
+//    public String reservation(){
+//        return "reservation";
+//    }
+//
+//
+//
+//
+//    //출석체크
+//    @GetMapping(value = "/gym-mypage/check")
+//    public String check(){
+//        return "check";
+//    }
+//
+//
+//    //마이페이지
+//    @GetMapping(value = "/trainer-mypage")
+//    public String trainerMypage(){
+//        return "trainer-mypage";
+//    }
+//
+//    @GetMapping(value = "/user-mypage")
+//    public String userMypage(){
+//        return "user-mypage";
+//    }
+//
+//    @GetMapping(value = "/gym-mypage")
+//    public String gymMypage(){
+//        return "gym-mypage";
+//    }
+//
+//
+//
+//
+//    //등록*생성 (시설,트레이너,상품)
+//    @GetMapping(value = "/user-mypage/new-gym")
+//    public String newGym(){
+//        return "new-gym";
+//    }
+//
+//    @GetMapping(value = "/user-mypage/new-trainer")
+//    public String newTrainer(){
+//        return "new-trainer";
+//    }
+//
+//    @GetMapping(value = "/gym-mypage/new-product")
+//    public String newProductByGym(){
+//        return "new-product";
+//    }
+//
+//    @GetMapping(value = "/trainer-mypage/new-product")
+//    public String newProductByTrainer(){
+//        return "new-product";
+//    }
+//
+//}
