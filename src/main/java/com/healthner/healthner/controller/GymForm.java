d264e46ac6d9226049ed491b3e78639b2e516155
Jogeonhee <ghj171937@gmail.com>
2021-04-09 03:27:17 +0900

    feat: gym 등록 기능 수정 GymForm 수정 RegisterController 수정 GymRepository 수정

--- src/main/java/com/healthner/healthner/controller/GymForm.java
+++ src/main/java/com/healthner/healthner/controller/GymForm.java
@@ -1,10 +1,12 @@
 package com.healthner.healthner.controller;
 
+import com.healthner.healthner.domain.Gym;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class GymForm {
 
@@ -12,4 +14,8 @@ public class GymForm {
     private String address;
     private String content;
     private String businessNumber;
+
+    public Gym getEntity(GymForm gymForm) {
+        return Gym.createGym(gymForm.getName(), gymForm.getAddress(), gymForm.getContent(), getBusinessNumber(), null);
+    }
 }
--- src/main/java/com/healthner/healthner/controller/RegisterController.java
+++ src/main/java/com/healthner/healthner/controller/RegisterController.java
@@ -2,22 +2,27 @@ package com.healthner.healthner.controller;
 
 import com.healthner.healthner.domain.Gym;
 import com.healthner.healthner.repository.GymRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.awt.*;
+import java.util.List;
+import java.util.ArrayList;
+
 
 @Controller
+@RequiredArgsConstructor
 public class RegisterController {
 
-    @Autowired
-    GymRepository gymRepository;
+//    @Autowired
+    private final GymRepository gymRepository;
+
+//    @Autowired
+//    public RegisterController(GymRepository gymRepository) {
+//        this.gymRepository = gymRepository;
+//    }
 
     @GetMapping(value = "/user-mypage/new-gym")
     public String register(Model model){
@@ -26,9 +31,13 @@ public class RegisterController {
     }
 
     @PostMapping(value = "/user-mypage/new-gym")
-    public String register(@RequestBody Gym gym){
-        Gym newGym = Gym.createGym(gym.getName(), gym.getAddress(), gym.getContent(), gym.getBusinessNumber(),null);
-        gymRepository.save(newGym);
+    public String register(@ModelAttribute GymForm gymForm) {
+
+        Gym gym = gymForm.getEntity(gymForm);
+//        ModelMapper modelMapper = new ModelMapper();
+//        GymForm newGym = modelMapper.map(gym,GymForm.class);
+//        Gym gym = modelMapper.map(gymForm, Gym.class);
+        gymRepository.save(gym);
         return "/gym-mypage";
     }
 }
