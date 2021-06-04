8595895a46fa85fb0454d16fa0976fa6bf34ee42
Jogeonhee <ghj171937@gmail.com>
2021-06-04 16:20:48 +0900

    deploy: 배포 스크립트 작성

---
+++ deploy/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#기존에 존재하는 컨테이너 정지
+docker stop `docker ps -qa`
+#기존에 존재하는 컨테이너 삭제
+docker rm `docker ps -qa`
+#기존에 존재하는 이미지 삭제
+docker rmi `docker images -qa`
+#새로운 컨테이너 실행
+docker-compose up -d
